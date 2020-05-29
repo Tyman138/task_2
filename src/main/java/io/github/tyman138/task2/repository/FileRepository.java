382569836d7971656959560a9c5c7fc92144a033
Tyman138 <48792513+Tyman138@users.noreply.github.com>
2020-05-29 13:23:48 +0300

    update: little fix

--- src/main/java/io/github/tyman138/task2/repository/FileRepository.java
+++ src/main/java/io/github/tyman138/task2/repository/FileRepository.java
@@ -2,8 +2,7 @@ package io.github.tyman138.task2.repository;
 
 import io.github.tyman138.task2.entity.DataBaseInfoFile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface FileRepository extends JpaRepository<DataBaseInfoFile, Long> {
 }
