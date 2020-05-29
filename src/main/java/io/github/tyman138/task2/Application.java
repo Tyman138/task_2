5d2bf98682c990aaee62b846ee06df5e04bbea64
Tyman138 <48792513+Tyman138@users.noreply.github.com>
2020-05-29 13:23:22 +0300

    update: little fix

--- src/main/java/io/github/tyman138/task2/Application.java
+++ src/main/java/io/github/tyman138/task2/Application.java
@@ -1,13 +1,15 @@
 package io.github.tyman138.task2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
