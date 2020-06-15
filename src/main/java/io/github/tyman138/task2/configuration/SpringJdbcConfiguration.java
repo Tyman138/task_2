b8c39b4a7ad38e4fe5d755c0733591b418802e2d
Tyman138 <48792513+Tyman138@users.noreply.github.com>
2020-06-15 14:08:09 +0300

    feat : getting datasource from application.yml

--- src/main/java/io/github/tyman138/task2/configuration/SpringJdbcConfiguration.java
+++ src/main/java/io/github/tyman138/task2/configuration/SpringJdbcConfiguration.java
@@ -2,25 +2,23 @@ package io.github.tyman138.task2.configuration;
 
 import io.github.tyman138.task2.dao.CarDao;
 import io.github.tyman138.task2.dao.impl.CarDaoImpl;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringJdbcConfiguration {
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/cars");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("root");
-        return dataSource;
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
-    public CarDao carDao(){
+    public CarDao carDao() {
         return new CarDaoImpl(dataSource());
     }
 }
