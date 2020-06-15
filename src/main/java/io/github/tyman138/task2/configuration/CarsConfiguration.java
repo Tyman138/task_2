58d9731870298eabacb7781d1c29faecd933a5db
Tyman138 <48792513+Tyman138@users.noreply.github.com>
2020-06-15 14:06:14 +0300

    fix : formatting

--- src/main/java/io/github/tyman138/task2/configuration/CarsConfiguration.java
+++ src/main/java/io/github/tyman138/task2/configuration/CarsConfiguration.java
@@ -2,20 +2,14 @@ package io.github.tyman138.task2.configuration;
 
 import io.github.tyman138.task2.controller.CarsController;
 import io.github.tyman138.task2.dao.CarDao;
-import io.github.tyman138.task2.dao.impl.CarDaoImpl;
 import io.github.tyman138.task2.repository.CarRepository;
 import io.github.tyman138.task2.repository.FileRepository;
 import io.github.tyman138.task2.service.CarService;
 import io.github.tyman138.task2.service.FileService;
 import io.github.tyman138.task2.service.impl.CarServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @Configuration
 //@EnableJpaRepositories(basePackages = "io.github.tyman138.task2.repository",repositoryBaseClass = io.github.tyman138.task2.repository.impl.DefaultRepositoryImpl.class)
@@ -43,7 +37,7 @@ public class CarsConfiguration {
 
     @Bean
     public CarsController carsController() {
-        return new CarsController(carService, fileService,carDao);
+        return new CarsController(carService, fileService, carDao);
     }
 
 }
